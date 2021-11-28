834104450ee5580dd2b68a4321deeb328c58e576
DevBloo <devchanghwan@gmail.com>
2021-11-28 12:39:09 +0900

    update: default setting update

--- src/main/java/com/hanghae/api/HanghaeDeliveryApplication.java
+++ src/main/java/com/hanghae/api/HanghaeDeliveryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class HanghaeDeliveryApplication {
 
     public static void main ( String[] args ) {
-        SpringApplication.run(ApiApplication.class, args);
+        SpringApplication.run(HanghaeDeliveryApplication.class, args);
     }
 
 }
